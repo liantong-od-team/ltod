dc2914bca91b228ff51477ba0df3a046c2ccf0d0
marsfun <stin.fly@gmail.com>
2015-08-13 12:29:29 +0800

    udpate autorun shell & jdbc

--- src/main/java/com/boco/od/utils/DsFactory.java
+++ src/main/java/com/boco/od/utils/DsFactory.java
@@ -7,6 +7,7 @@ import javax.sql.DataSource;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
@@ -21,7 +22,7 @@ public class DsFactory {
     private NamedParameterJdbcTemplate jdbc;
     private static final String PROPERTIES_PATH = "/conf/jdbc.properties";
     private static final String EMPTY = "";
-    static String driver, url, username, password;
+    static String driver, url, username, password, initialSize, minIdle, maxActive;
 
     private static DsFactory dsins = new DsFactory();
 
@@ -31,26 +32,32 @@ public class DsFactory {
         url = prop.getProperty("oracle.url", EMPTY);
         username = prop.getProperty("oracle.username", EMPTY);
         password = prop.getProperty("oracle.password", EMPTY);
-
+        initialSize = prop.getProperty("oracle.initial", EMPTY);
+        minIdle = prop.getProperty("oracle.minIdle", EMPTY);
+        maxActive = prop.getProperty("oracle.maxActive", EMPTY);
 
         Properties p = new Properties();
         p.setProperty("driverClassName", driver);
         p.setProperty("url", url);
         p.setProperty("username", username);
         p.setProperty("password", password);
-        p.setProperty("initialSize", "1");
-        p.setProperty("minIdle", "1");
-        p.setProperty("maxActive", "10");
+        p.setProperty("initialSize", initialSize);
+        p.setProperty("minIdle", minIdle);
+        p.setProperty("maxActive", maxActive);
 
         System.out.println("driverClassName=" + driver);
         System.out.println("url=" + url);
         System.out.println("username=" + username);
         System.out.println("password=" + password);
+        System.out.println("initialSize=" + initialSize);
+        System.out.println("minIdle=" + minIdle);
+        System.out.println("maxActive=" + maxActive);
         try {
             ds = createDataSource(p);
             jdbc = new NamedParameterJdbcTemplate(ds);
         } catch (Exception e) {
             e.printStackTrace();
+            System.err.println(e.getMessage());
         }
 
     }
@@ -60,18 +67,78 @@ public class DsFactory {
     }
 
 
-    private String  sql = "select name from (select m.name,SDO_GEOM.RELATE(GEOM,'Contains', MDSYS.SDO_GEOMETRY(2001, 8307, MDSYS.SDO_POINT_TYPE(%s, %s, 0), NULL, NULL), 0.5) status from %s m) where status = 'CONTAINS'";
+    private String sql_province = "select name from (select m.name,SDO_GEOM.RELATE(GEOM,'Contains', MDSYS.SDO_GEOMETRY(2001, 8307, MDSYS.SDO_POINT_TYPE(%s, %s, 0), NULL, NULL), 0.5) status from GIS_PROVINCE m) where status = 'CONTAINS'";
+    private String sql_country = "select name from (select m.name,SDO_GEOM.RELATE(GEOM,'Contains', MDSYS.SDO_GEOMETRY(2001, 8307, MDSYS.SDO_POINT_TYPE(%s, %s, 0), NULL, NULL), 0.5) status from GIS_COUNTRY m) where status = 'CONTAINS'";
 
 
-
+    private boolean checkParams(String longitude, String latitude) {
+        double dlng, dlat;
+        try {
+            dlng = Double.valueOf(longitude);
+            dlat = Double.valueOf(latitude);
+        } catch (Exception ex) {
+            return false;
+        }
+        if (dlng <= 180 && dlng >= -180 && dlat <= 90 && dlat >= -90) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     public String queryProvince(String longitude, String latitude) {
+        if(!checkParams(longitude,latitude)) {
+            System.out.println("check failed.");
+            return "";
+        }
+        String provSql = String.format(sql_province, ":lng", ":lat");
+        Map<String, Double> paramArr = new HashMap<String, Double>();
+        paramArr.put("lng", Double.valueOf(longitude));
+        paramArr.put("lat", Double.valueOf(latitude));
+//        System.out.println("provSql=" + provSql);
+        List<Map<String,Object>> lst = jdbc.queryForList(provSql, paramArr);
+        if (lst.size() > 0) {
+            return String.valueOf(lst.get(0).get("name"));
+        } else {
+            return "";
+        }
+    }
 
-        String provSql = String.format(sql,":lng",":lat",":tbl");
+    public String queryCountry(String longitude, String latitude) {
+        if(!checkParams(longitude,latitude)){
+            System.out.println("check failed.");
+            return "";
+        }
+        String countrySql = String.format(sql_country, ":lng", ":lat");
+        Map<String, Double> paramArr = new HashMap<String, Double>();
+        paramArr.put("lng", Double.valueOf(longitude));
+        paramArr.put("lat", Double.valueOf(latitude));
+//        System.out.println("countrySql=" + countrySql);
+        List<Map<String,Object>> lst = jdbc.queryForList(countrySql, paramArr);
+        if (lst.size() > 0) {
+            return String.valueOf(lst.get(0).get("name"));
+        } else {
+            return "";
+        }
+    }
+
+    public String[] queryAll(String longitude, String latitude) {
+        String p = queryProvince(longitude, latitude);
+        String c = queryCountry(longitude, latitude);
+        return new String[]{p, c};
+    }
+
+
+/*
+
+
+
+    public String queryProvince0(String longitude, String latitude) {
+
+        String provSql = String.format(sql_province, ":lng", ":lat");
         Map<String, String> paramArr = new HashMap<String, String>();
         paramArr.put("lng", longitude);
         paramArr.put("lat", latitude);
-        paramArr.put("tbl", "GIS_PROVINCE");
 
         String r = jdbc.queryForObject(provSql, paramArr, new RowMapper<String>() {
             @Override
@@ -83,12 +150,11 @@ public class DsFactory {
         return r;
     }
 
-    public String queryCountry(String longitude, String latitude) {
-        String countrySql = String.format(sql,":lng",":lat",":tbl");
+    public String queryCountry0(String longitude, String latitude) {
+        String countrySql = String.format(sql_country, ":lng", ":lat");
         Map<String, String> paramArr = new HashMap<String, String>();
         paramArr.put("lng", longitude);
         paramArr.put("lat", latitude);
-        paramArr.put("tbl", "GIS_COUNTRY");
         String r = jdbc.queryForObject(countrySql, paramArr, new RowMapper<String>(){
             @Override
             public String mapRow(ResultSet rs, int i) throws SQLException {
@@ -100,23 +166,24 @@ public class DsFactory {
         return r;
 
     }
-
-    public String[] queryAll(String longitude, String latitude) {
-        String p = queryProvince(longitude, latitude);
-        String c = queryCountry(longitude, latitude);
+    public String[] queryAll0(String longitude, String latitude) {
+        String p = queryProvince0(longitude, latitude);
+        String c = queryCountry0(longitude, latitude);
         return new String[]{p, c};
     }
+*/
+
+
 
 
     public static void main(String[] args) {
         String lng = "116.372639";
         String lat = "39.924627";
-        System.out.println(DsFactory.getinstance().queryProvince(lng, lat));
-        System.out.println(DsFactory.getinstance().queryCountry(lng, lat));
-        for(String s: DsFactory.getinstance().queryAll(lng, lat)){
+        System.out.println("res = " + DsFactory.getinstance().queryProvince(lng, lat));
+        System.out.println("res = " + DsFactory.getinstance().queryCountry(lng, lat));
+        for (String s : DsFactory.getinstance().queryAll(lng, lat)) {
             System.out.println(s);
         }
-
 
 
     }
--- src/runtime/od.sh
+++ src/runtime/od.sh
@@ -4,61 +4,78 @@ jar_path="."
 jar_name="od-assembly-1.0.jar"
 
 #ods
-ODS_Cell_OD_Track_indir="odsin"
-ODS_Cell_OD_Track_outdir="odsout"
+ODS_Cell_OD_Track_indir="hdfs://boh/user/hive/warehouse/zba_dwa.db/zb_d_yd_voice_013/*/*/*/*/*"
+ODS_Cell_OD_Track_outdir="/sunwukongtmp/odtest/autorun/odoutl1"
+ODS_Cell_OD_Track_outdir2="/sunwukongtmp/odtest/autorun/odoutl2"
+
+dim_cell="/sunwukongtmp/dim_all_station_cellid_new/"
+dim_lrc="hdfs://boh/user/hive/warehouse/zba_dwa.db/dim_msisdn_seg/"
+
+#ODS_Cell_OD_Track_indir="fy/lt/table"
+#ODS_Cell_OD_Track_outdir="fy/lt/autorun/odsoutl1"
+#ODS_Cell_OD_Track_outdir2="fy/lt/autorun/odsoutl2"
+#
+#dim_cell="fy/lt/dim/dim_all_station_cellid_new"
+#dim_lrc="fy/lt/dim/dim_msisdn_seg"
+
+
+
+
 #dw
-DW_City_OD_Track_outdir="dwcityout"
-DW_County_OD_Track_outdir="dwcountryout"
+DW_City_OD_Track_outdir="/sunwukongtmp/odtest/autorun/dwcityout"
+DW_County_OD_Track_outdir="/sunwukongtmp/odtest/autorun/dwcountryout"
+
+#DW_City_OD_Track_outdir="fy/lt/autorun/dwcityout"
+#DW_County_OD_Track_outdir="fy/lt/autorun/dwcountryout"
 
 
 ODS_Cell_OD_Track(){
-	echo "hadoop jar ${jar_path}/${jar_name} com.boco.od.ods.CellOdTraceDriver ${ODS_Cell_OD_Track_indir} ${ODS_Cell_OD_Track_outdir}"
-    hadoop jar ${jar_path}/${jar_name} com.boco.od.ods.CellOdTraceDriver ${ODS_Cell_OD_Track_indir} ${ODS_Cell_OD_Track_outdir}
+	echo "hadoop jar ${jar_path}/${jar_name} com.boco.od.ods.CellOdTraceDriverLocal ${ODS_Cell_OD_Track_indir} ${ODS_Cell_OD_Track_outdir} ${dim_cell} ${dim_lrc} dim_all_station_cellid_new DIM_MSISDN_SEG"
+    hadoop jar ${jar_path}/${jar_name} com.boco.od.ods.CellOdTraceDriverLocal ${ODS_Cell_OD_Track_indir} ${ODS_Cell_OD_Track_outdir} ${dim_cell} ${dim_lrc} dim_all_station_cellid_new DIM_MSISDN_SEG
+
+    echo "hadoop jar ${jar_path}/${jar_name} com.boco.od.ods.XDriver ${ODS_Cell_OD_Track_outdir} ${ODS_Cell_OD_Track_outdir2}"
+    hadoop jar ${jar_path}/${jar_name} com.boco.od.ods.XDriver ${ODS_Cell_OD_Track_outdir} ${ODS_Cell_OD_Track_outdir2}
+
+
 }
 
 DW_City_OD_Track(){
-	inputdir=${ODS_Cell_OD_Track_outdir}
+	inputdir=${ODS_Cell_OD_Track_outdir2}
 	echo "hadoop jar ${jar_path}/${jar_name} com.boco.od.location.LocationOdDriver $inputdir ${DW_City_OD_Track_outdir} city gbk"
 	hadoop jar ${jar_path}/${jar_name} com.boco.od.location.LocationOdDriver $inputdir ${DW_City_OD_Track_outdir} city gbk
 }
 
 DW_County_OD_Track(){
-	inputdir=${ODS_Cell_OD_Track_outdir}
+	inputdir=${ODS_Cell_OD_Track_outdir2}
 	echo "hadoop jar ${jar_path}/${jar_name} com.boco.od.location.LocationOdDriver $inputdir ${DW_County_OD_Track_outdir} region gbk"
 	hadoop jar ${jar_path}/${jar_name} com.boco.od.location.LocationOdDriver $inputdir ${DW_County_OD_Track_outdir} region gbk
 }
-#TODO
+
 DW_Live_Work_15Day(){
-    inputdir=${ODS_Cell_OD_Track_outdir}
-	echo "hadoop jar ${jar_path}/${jar_name} com.boco.od.location.LocationOdDriver $inputdir ${DW_County_OD_Track_outdir} region gbk"
-	#hadoop jar ${jar_path}/${jar_name} com.boco.od.location.LocationOdDriver $inputdir ${DW_County_OD_Track_outdir} region gbk
+    shift
+	echo "hadoop jar ${jar_path}/${jar_name} com.boco.od.location.LiveWorkDriver $*"
+	hadoop jar ${jar_path}/${jar_name} com.boco.od.location.LiveWorkDriver $*
 
 }
 
 case "$1" in
 	'ods')
-	#nohup java $JAVA_OPTS -cp $JARS com.boco.sysadmin.app.SysadminApp $1 >> ${COMM_HOME}/bin/nohup.out &
-	#echo $! >> pidfile
+
 	echo "step => calc ODS_Cell_OD_Track"
 	ODS_Cell_OD_Track
 	;;
 	'dwcity')
-	#nohup java $JAVA_OPTS -cp $JARS com.boco.sysadmin.app.SysadminApp $2 >> ${COMM_HOME}/bin/nohup.out &
 	echo "step => calc DW_City_OD_Track"
 	DW_City_OD_Track
 	;;
    'dwregion')
-	#nohup java $JAVA_OPTS -cp $JARS com.boco.sysadmin.app.SysadminApp $2 >> ${COMM_HOME}/bin/nohup.out &
 	echo "step => calc DW_County_OD_Track"
 	DW_County_OD_Track
 	;;
 	'dw15d')
-	#rm ${COMM_HOME}/log/*
-	#rm ${COMM_HOME}/bin/nohup.out
-	#echo "stop finished"
-	#ps -ef | grep sysadmin|grep -v grep|awk '{print $2}'|xargs -i kill -9 {}
     echo "step => calc DW_Live_Work_15Day"
-    DW_Live_Work_15Day
+    DW_Live_Work_15Day $*
+
 	;;
 	*)
 	echo "ltod , boco ltd. (c)2010-2015"
